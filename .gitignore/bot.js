cefbdc39b37d952366276a03ba6a12626e5dcc03
Hexiss <75497497+Hexiss@users.noreply.github.com>
2020-12-04 19:32:14 +0100

    Add files via upload

---
+++ .gitignore/bot.js
@@ -0,0 +1,72 @@
+const CommandoClient = require('./client');
+const path = require('path');
+const fs = require('fs')
+const dotenv = require('dotenv')
+
+const envConfig = dotenv.parse(fs.readFileSync('.env'))
+for (const k in envConfig) {
+    process.env[k] = envConfig[k]
+}
+
+const client = new CommandoClient({
+    commandPrefix: 'w.',
+    owner: process.env.BOT_OWNER_ID,
+    disableMentions: 'everyone',
+    presence: {
+        activity: {
+            name: `Le père castor`, // message de présence
+            type: 'LISTENING' // type d'activité
+        }
+    }
+});
+
+/// AUTO REACT /// test server
+client.on('message', message => {
+    if (message.channel.id === "781531711530991626") {
+        message.react('👍');
+        message.react('👎');
+        message.author.send("Ta suggestion a bien été prise en compte, l'équipe **Wildsens** te remercie pour ton investissement ! :heart:"); 
+    }
+});
+/// AUTO REACT ///
+
+/// AUTO REACT /// wildsens
+client.on('message', message => {
+    if (message.channel.id === "779729352505688074") {
+        message.react('👍');
+        message.react('👎');
+        message.author.send("Ta suggestion a bien été prise en compte, l'équipe **Wildsens** te remercie pour ton investissement ! :heart:"); 
+    }
+});
+/// AUTO REACT ///
+
+fs.readdir('./events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach((file) => {
+        const eventFunction = require(`./events/${file}`);
+        if (eventFunction.disabled) return;
+
+        const event = eventFunction.event || file.split('.')[0];
+        const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client;
+        const { once } = eventFunction;
+
+        try {
+            emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.run(client, ...args));
+        } catch (error) {
+            console.error(error.stack);
+        }
+    });
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['divers', 'Divers'],
+        ['admin', 'Admin'],
+        ['bot', 'Bot'],
+
+    ])
+    .registerCommandsIn(path.join(__dirname, 'commands'))
+;
+
+client.login(process.env.BOT_TOKEN);
---
+++ .gitignore/commands/divers/hello.js
@@ -0,0 +1,25 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class HelloCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'hello',
+			memberName: 'hello',
+			group: 'divers',
+			aliases: ['bonjour', 'hi'],
+			description: 'Replies with a hello message.',
+			clientPermissions: ['SEND_MESSAGES'], // le bot doit avoir la permission d'envoyer des messages
+			userPermissions: ['ADMINISTRATOR'], // l'utilisateur doit être administrateur pour exécuter la commande
+//			ownerOnly: true, // Uniquement le propriétaire du bot pourra lancer cette commande
+	                guildOnly: false,
+	                throttling: {
+	                        usages: 2,
+	                        duration: 10,
+	                },
+		});
+	}
+
+	async run(msg) {
+            msg.say(`Bonjour, je suis ${this.client.user.tag} (\`${this.client.user.id}\`)`);
+	}
+};
